c1007c8e92751c37e1704f448541763d76374605
Maksim-07 <kosmakovmaksim@yandex.ru>
2025-05-16 21:08:11 +0300

    Add endpoint GET fcm-token

--- src/api/v1/fcm_token.py
+++ src/api/v1/fcm_token.py
@@ -1,10 +1,27 @@
+from typing import Sequence
+
 from fastapi import APIRouter, Depends, status
 
-from core.auth import get_user_id_from_any_token
-from schemas.fcm_token import FCMTokenSchema
+from core.auth import (
+    get_user_id_from_any_token,
+    verify_admin_token_from_header,
+)
+from schemas.fcm_token import FCMTokenSchema, GetFCMTokenSchema
 from services.fcm_token import FCMTokenService
 
 router = APIRouter(prefix="/fcm-token", tags=["FCM Token"])
+
+
+@router.get(
+    "",
+    status_code=status.HTTP_200_OK,
+    response_model=Sequence[GetFCMTokenSchema],
+    dependencies=[Depends(verify_admin_token_from_header)],
+)
+async def get_tokens(
+    fcm_token_service: FCMTokenService = Depends(),
+) -> Sequence[GetFCMTokenSchema]:
+    return await fcm_token_service.get_tokens()
 
 
 @router.post("", status_code=status.HTTP_200_OK, response_model=None)
